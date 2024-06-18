c00ff9884b387d8b73b8033c29f1ff29698393c4
Ewen McNeill <ewen@naos.co.nz>
2024-06-18 20:13:50 +1200

    log: add logging colours
    To distinguish the emulator log messages from the Plexus P/20 technician
    log messages.

--- log.c
+++ log.c
@@ -25,14 +25,33 @@ int log_channel_verbose_level[] = {
 	LOG_EMU_DEFAULT_LEVEL
 };
 
+// "ANSI" colour escape sequences
+#define ANSI_COLOUR_NORMAL       "\033[0m"
+#define ANSI_COLOUR_RED          "\033[31m"
+#define ANSI_COLOUR_GREEN        "\033[32m"
+#define ANSI_COLOUR_YELLOW       "\033[33m"
+#define ANSI_COLOUR_MAGENTA      "\033[35m"
+#define ANSI_COLOUR_GREY         "\033[37m"
+
+// These must be in teh same order as enum log_level (in log.h)
+const char *log_level_colour[] = {
+	ANSI_COLOUR_RED,     // LOG_ERR     (0)
+	ANSI_COLOUR_MAGENTA, // LOG_WARNING (1)
+	ANSI_COLOUR_YELLOW,  // LOG_NOTICE  (2)
+	ANSI_COLOUR_GREEN,   // LOG_INFO    (3)
+	ANSI_COLOUR_GREY     // LOG_DEBUG   (4)
+};
+
 int log_printf(enum log_source source, enum log_level msg_level, const char *format, ...) {
         va_list ap;
         int printed = 0;
 
         if (log_channel_verbose_level[source] >= msg_level) {
-                va_start(ap, format);
-                printed = vprintf(format, ap);
-                va_end(ap);
+		printf("%s", log_level_colour[msg_level]);
+		va_start(ap, format);
+		printed = vprintf(format, ap);
+		va_end(ap);
+		printf("%s", ANSI_COLOUR_NORMAL);
         }
         return printed;
 }
