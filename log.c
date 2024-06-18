7e0ab3c25f97753ab293eb41b30af129a0806309
Ewen McNeill <ewen@naos.co.nz>
2024-06-18 15:40:36 +1200

    uart: abstract debug logging to wrapper function
    Centralised debug logging in log.[ch], but so far only
    uart.c has been converted to use it.

---
+++ log.c
@@ -0,0 +1,22 @@
+#include "log.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+// Default log levels for log sources
+#define LOG_UART_DEFAULT_LEVEL LOG_ERR
+
+int log_channel_verbose_level[] = {
+	LOG_UART_DEFAULT_LEVEL
+};
+
+int log_printf(enum log_source source, int msg_level, const char *format, ...) {
+        va_list ap;
+        int printed = 0;
+
+        if (log_channel_verbose_level[source] >= msg_level) {
+                va_start(ap, format);
+                printed = vprintf(format, ap);
+                va_end(ap);
+        }
+        return printed;
+}
---
+++ log.h
@@ -0,0 +1,19 @@
+#ifndef LOG_H
+#define LOG_H
+
+// Log sources (should be numbered sequentially from 0)
+enum log_source {
+	LOG_SRC_UART = 0
+};
+
+
+// Emulator log levels (higher is more verbose)
+#define LOG_ERR        0
+#define LOG_WARNING    1
+#define LOG_NOTICE     2
+#define LOG_INFO       3
+#define LOG_DEBUG      4
+
+int log_printf(enum log_source channel, int msg_level, const char *format, ...);
+
+#endif
